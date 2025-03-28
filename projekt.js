86d693e9e6c3befe5c7cea5744c975f2868b514e
Jimgonhxx <aluqsquad@gmail.com>
2025-03-28 20:27:12 +0100

    Zmiany projektu(bootstrap,możliwośc wybrania jednego checkboxa w js, poprawki stylu)

--- projekt.js
+++ projekt.js
@@ -1,7 +1,19 @@
 
 // nawigacja do strony projekt2.html
 // Przykład użycia JavaScript do nawigacji do innej strony
-
 document.getElementById("button").addEventListener("click", function() {
     window.location.href = "projekt2.html";
+});
+
+// Skrypt do wybierania tylko jednego checkboxa w grupie (np. płeć)
+document.querySelectorAll('.gender-checkbox').forEach(checkbox => {
+    checkbox.addEventListener('change', (event) => {
+        if (event.target.checked) {
+            document.querySelectorAll('.gender-checkbox').forEach(cb => {
+                if (cb !== event.target) {
+                    cb.checked = false; // Odznacz inne checkboxy
+                }
+            });
+        }
+    });
 });

37568b3acc6065b4df24c30e649b8c615f398bc6
Jimgonhxx <aluqsquad@gmail.com>
2025-03-28 19:56:04 +0100

    Poprawienie stylu

--- projekt.js
+++ projekt.js
@@ -1,8 +1,6 @@
 
-
-
-
-
+// nawigacja do strony projekt2.html
+// Przykład użycia JavaScript do nawigacji do innej strony
 
 document.getElementById("button").addEventListener("click", function() {
     window.location.href = "projekt2.html";
